1cf205e61f104559376762d3106e1f99996c3f75
Pablo Romo <djromo2296@gmail.com>
2023-10-31 19:27:19 -0600

    header agregado

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './components/Header/Header'
 import './App.css'
 import { Dashboard, Home, Login, Secret, SignUp } from '@/pages'
 
 function App () {
   return (
     <>
+      <Header />
       <Home />
       <Dashboard />
       <Login />
