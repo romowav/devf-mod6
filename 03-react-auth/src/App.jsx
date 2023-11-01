b42996a5da6ff3ca1f1d0830d54f195aa3cadfbd
Pablo Romo <djromo2296@gmail.com>
2023-10-31 19:08:37 -0600

    creacion de pages

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Secret from './pages/Secret'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <>
-      
+      <Home />
+      <Dashboard />
+      <Login />
+      <Secret />
+      <SignUp />
     </>
   )
 }
