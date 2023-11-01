2a9755c4a3a6fbb790d4203b570ff537ea0b73f1
Pablo Romo <djromo2296@gmail.com>
2023-10-31 19:14:40 -0600

    archivo index con imports de pages

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Secret from './pages/Secret'
-import SignUp from './pages/SignUp'
+import { Dashboard, Home, Login, Secret, SignUp } from '@/pages'
 
-function App() {
+function App () {
   return (
     <>
       <Home />
---
+++ 03-react-auth/src/pages/index.js
@@ -0,0 +1,7 @@
+import Dashboard from '@/pages/Dashboard'
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import Secret from '@/pages/Secret'
+import SignUp from '@/pages/SignUp'
+
+export { Dashboard, Home, Login, Secret, SignUp }
