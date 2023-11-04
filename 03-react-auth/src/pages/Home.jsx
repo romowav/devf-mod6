1de2fa4b0b0cc645a2491b67ad756b4bbbc0a0fc
Pablo Romo <djromo2296@gmail.com>
2023-11-04 16:24:18 -0600

    Llamado de Productos de la API

--- 03-react-auth/src/pages/Home.jsx
+++ 03-react-auth/src/pages/Home.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react'
+import { getAllItemService } from '@/services/itemServices'
+
 const Home = () => {
+  const [itemData, setItemData] = useState([])
+
+  useEffect(() => {
+    const getItemData = async () => {
+      try {
+        const response = await getAllItemService()
+        if (response.status === 200) {
+          setItemData(response.data)
+        }
+      } catch (error) {
+        console.log('Ocurrio un error en Dashboard', error.message)
+      }
+    }
+    getItemData()
+  }, [token])
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      {/* Crear logica para crear cards con informacion de cada item que ya estamos llamando de la API */}
+    </>
   )
 }
 
--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -4,5 +4,6 @@ const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getMeUserService = (jwtToken) => axios.get(`${BASE_URL}/users/me`, { headers: { Authorization: `Bearer ${jwtToken}` } })
 
-export { registerUserService, loginUserService }
+export { registerUserService, loginUserService, getMeUserService }
