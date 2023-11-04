bb173b590af7ab9efa96a66e8fdc39c39f858f0d
Pablo Romo <djromo2296@gmail.com>
2023-11-04 15:15:28 -0600

    proteccion de rutas

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '@/hooks/useAuthContext'
 import './Header.scss'
 
 const Header = () => {
+  const { isAuth, logout } = useAuthContext()
+
   const linkIsActive = (isActive) => {
     return isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
   }
@@ -12,20 +15,44 @@ const Header = () => {
       </NavLink>
       <ul className='header__nav-list'>
         <li className='headre__list-item'>
-          <NavLink to='/' className={({ isActive }) => linkIsActive(isActive)}>Home</NavLink>
+          <NavLink to='/' className={({ isActive }) => linkIsActive(isActive)}>
+            Home
+          </NavLink>
         </li>
-        <li className='headre__list-item'>
-          <NavLink to='/dashboard' className={({ isActive }) => linkIsActive(isActive)}>Dashboard</NavLink>
+        <li className='header__list-item'>
+          <NavLink to='/dashboard' className={({ isActive }) => linkIsActive(isActive)}>
+            Dashboard
+          </NavLink>
         </li>
-        <li className='headre__list-item'>
-          <NavLink to='/login' className={({ isActive }) => linkIsActive(isActive)}>Login</NavLink>
-        </li>
-        <li className='headre__list-item'>
-          <NavLink to='/secret' className={({ isActive }) => linkIsActive(isActive)}>Secret</NavLink>
-        </li>
-        <li className='headre__list-item'>
-          <NavLink to='/signup' className={({ isActive }) => linkIsActive(isActive)}>SignUp</NavLink>
-        </li>
+        {isAuth
+          ? (
+            <>
+              <li className='headre__list-item'>
+                <NavLink to='/secret' className={({ isActive }) => linkIsActive(isActive)}>
+                  Secret
+                </NavLink>
+              </li>
+              <li className='header__list-item'>
+                <NavLink to='/' onClick={logout} className='header__item-link'>
+                  Logout
+                </NavLink>
+              </li>
+            </>
+            )
+          : (
+            <>
+              <li className='header__list-item'>
+                <NavLink to='/login' className={({ isActive }) => linkIsActive(isActive)}>
+                  Login
+                </NavLink>
+              </li>
+              <li className='header__list-item'>
+                <NavLink to='/signup' className={({ isActive }) => linkIsActive(isActive)}>
+                  SignUp
+                </NavLink>
+              </li>
+            </>
+            )}
       </ul>
     </nav>
   )
--- 03-react-auth/src/routes/RoutesIndex.jsx
+++ 03-react-auth/src/routes/RoutesIndex.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Dashboard, Home, Login, Secret, SignUp } from '@/pages'
+import { useAuthContext } from '@/hooks/useAuthContext'
 
 const RoutesIndex = () => {
+  const { isAuth } = useAuthContext()
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-      <Route path='/secret' element={<Secret />} />
+      <Route path='/dashboard' element={isAuth ? <Dashboard /> : <Navigate to='/login' />} />
+      <Route path='/secret' element={isAuth ? <Secret /> : <Navigate to='/login' />} />
     </Routes>
   )
 }
